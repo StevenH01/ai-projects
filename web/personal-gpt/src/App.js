0d0c9daebfdabb713be3425bc36bf0ad0ae86f2a
Steven Ho <steven09ho@gmail.com>
2024-02-29 17:12:34 -0800

    commiting for personal chat gpt project

---
+++ web/personal-gpt/src/App.js
@@ -0,0 +1,17 @@
+import './normal.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+      <section className="chat-box">
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
